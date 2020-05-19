b91a639cd5a280a2bd64a8e946514d24bf89c0f9
Justus Rademacher <justus.berlin@gmail.com>
2020-05-19 19:15:09 +0200

    change delete

--- commands/delete.js
+++ commands/delete.js
@@ -3,17 +3,36 @@ module.exports = {
 	description: 'Lösche bis zu 99 Nachrichten.',
 	guildOnly: true,
 	execute(message, args) {
-		const amount = parseInt(args[0]) + 1;
+	
 		if (message.member.roles.has('697424990592696350')) {
-			if (isNaN(amount)) {
-				return message.reply('Das ist keine gültige Anzahl an Nachrichten');
-			} else if (amount <= 1 || amount > 100) {
-				return message.reply('Bitte wähle eine Anzahl zwischen 1 und 99 Nachrichten');
+			if(args[0]=='all'){
+				if (message.member.roles.has('697559808424804395' || message.member.roles.has('696828765996580984'))) {
+					message.channel.send("delete all")
+				}else{
+					message.channel.send("Nur Owner können das!!!")
+				}
+				
+				/* async () => {
+					let fetched;
+					do {
+					  fetched = await channel.fetchMessages({limit: 100});
+					  message.channel.bulkDelete(fetched);
+					}
+					while(fetched.size >= 2);
+				  } */
 			}
-			message.channel.bulkDelete(amount, true).catch(err => {
-				console.error(err);
-				message.channel.send('Fehler beim Löschen der Nachrichten!');
-			});
+			else{
+				const amount = parseInt(args[0]) + 1;
+				if (isNaN(amount)) {
+					return message.reply('Das ist keine gültige Anzahl an Nachrichten');
+				} else if (amount <= 1 || amount > 100) {
+					return message.reply('Bitte wähle eine Anzahl zwischen 1 und 99 Nachrichten');
+				}
+				message.channel.bulkDelete(amount, true).catch(err => {
+					console.error(err);
+					message.channel.send('Fehler beim Löschen der Nachrichten!');
+				});
+			}
 		}
 		else {
 			message.channel.send('Du hast keine Rechte um diesen Befehl zu nutzen!!!');
