91893be275c4d822d7e816efe10be2256487b977
Justus Rademacher <justus.rademacher@bwb.de>
2020-05-19 10:03:38 +0200

    change bot

--- bot.js
+++ bot.js
@@ -2,21 +2,92 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const prefix = "reborn!";
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+client.on("ready", () => {
+    // This event will run if the bot starts, and logs in, successfully.
+    console.log(`Bot has started.`); 
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  });
+  
+  
+  
+  
+  client.on("message", async message => {
+    // This event will run on every single message received, from any channel or DM.
 
- 
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
-});
+    // It's good practice to ignore other bots. This also makes your bot ignore itself
+    // and not get into a spam loop (we call that "botception").
+    if(message.author.bot) return;
+    
+    // Also good practice to ignore any message that does not start with our prefix, 
+    // which is set in the configuration file.
+    if(message.content.indexOf(prefix) !== 0) return;
+    
+    // Here we separate our "command" name, and our "arguments" for the command. 
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
+    // Let's go with a few common example commands! Feel free to delete or change those.
+    
+    if(command === "ping") {
+      // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+      // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+      const m = await message.channel.send("Ping?");
+      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    }
+    
+    if(command === "kick") {
+      // This command must be limited to mods and admins. In this example we just hardcode the role names.
+      // Please read on Array.some() to understand this bit: 
+      // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+      if(!message.member.roles.some(r=>["Owner","Dev"].includes(r.name)) )
+        return message.reply("Sorry, you don't have permissions to use this!");
+      
+      // Let's first check if we have a member and if we can kick them!
+      // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+      // We can also support getting the member by ID, which would be args[0]
+      let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+      if(!member)
+        return message.reply("Please mention a valid member of this server");
+      if(!member.kickable) 
+        return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+      
+      // slice(1) removes the first part, which here should be the user mention or ID
+      // join(' ') takes all the various parts to make it a single string.
+      let reason = args.slice(1).join(' ');
+      if(!reason) reason = "No reason provided";
+      
+      // Now, time for a swift kick in the nuts!
+      await member.kick(reason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+      message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+  
+    }
+    
+    if(command === "ban") {
+      // Most of this command is identical to kick, except that here we'll only let admins do it.
+      // In the real world mods could ban too, but this is just an example, right? ;)
+      if(!message.member.roles.some(r=>["Owner"].includes(r.name)) )
+        return message.reply("Sorry, you don't have permissions to use this!");
+      
+      let member = message.mentions.members.first();
+      if(!member)
+        return message.reply("Please mention a valid member of this server");
+      if(!member.bannable) 
+        return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+  
+      let reason = args.slice(1).join(' ');
+      if(!reason) reason = "No reason provided";
+      
+      await member.ban(reason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+      message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+    }
+    
+   
+  });
+  
 
 // THIS  MUST  BE  THIS  WAY
 
