10e9b16ae8c33a54b616544249733a562776f889
Justus Rademacher <justus.rademacher@bwb.de>
2020-05-19 12:10:17 +0200

    test

--- commands/prune.js
+++ commands/prune.js
@@ -10,7 +10,7 @@ module.exports = {
 		} else if (amount <= 1 || amount > 100) {
 			return message.reply('you need to input a number between 1 and 99.');
 		}
-		if (message.member.roles.has(697424990592696350)) {
+		if (message.member.roles.has('697424990592696350')) {
 			message.channel.bulkDelete(amount, true).catch(err => {
 				console.error(err);
 				message.channel.send('there was an error trying to prune messages in this channel!');
