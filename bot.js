b32efe82bb69788baead265e2a47bffa8451bdc6
Justus Rademacher <justus.rademacher@bwb.de>
2020-05-19 11:22:29 +0200

    bigg Change

--- bot.js
+++ bot.js
@@ -1,63 +1,75 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const prefix = "reborn!"
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix} = require('./config.json');
 
-client.on('message', (receivedMessage) => {
-    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
-        return
-    }
-    
-    if (receivedMessage.content.startsWith(prefix)) {
-        processCommand(receivedMessage)
-    }
-})
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-function processCommand(receivedMessage) {
-   
-    const args = receivedMessage.content.slice(prefix.length).split(' ');
-    const command = args.shift().toLowerCase();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-    switch (command){
-        case "help":
-            helpCommand(receivedMessage);
-            break;
-        case "dark":
-            darkAuction(receivedMessage);
-            break;
-        default:
-            receivedMessage.channel.send("Unbekannter Befehl. Der Befehl `!help` gibt dir eine Übersicht der verfügbaren Befehle.");
-            break;
-    }
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
 }
 
+const cooldowns = new Discord.Collection();
 
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
-function helpCommand(receivedMessage) {    
-        receivedMessage.channel.send("'!help' Für die Übersicht der Befehle \n '!dark' Für die verbleibende Zeit bis zur nächsten Dark Auction");
-}
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-function darkAuction(receivedMessage){
-    var stunden, minuten
-    var StundenZahl, MinutenZahl
-  
-    var heute = new Date()
-    StundenZahl = heute.getHours()
-    MinutenZahl = heute.getMinutes()
-   
-    stunden = StundenZahl+":";
-    if (MinutenZahl < 10) {minuten = "0" + MinutenZahl + ":"}
-      else {minuten = MinutenZahl }
-    var DarkAuctionTime =55
-    if(minuten>DarkAuctionTime){
-      var ZeitBisAuction=(60-minuten)+DarkAuctionTime;
-      receivedMessage.channel.send("Dark Auction ist in " + ZeitBisAuction + " Minuten")
-    }
-    if (minuten<DarkAuctionTime) {
-        var ZeitBisAuction=(DarkAuctionTime-minuten);
-        receivedMessage.channel.send("Dark Auction ist in " + ZeitBisAuction + " Minuten")
-    }
-}
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
 
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type !== 'text') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
 
 
  
