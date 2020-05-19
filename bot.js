b2ed317387dd41a32c67098288f06d31c11450e4
Justus Rademacher <justus.rademacher@bwb.de>
2020-05-19 10:57:00 +0200

    3.

--- bot.js
+++ bot.js
@@ -13,68 +13,28 @@ client.on('message', (receivedMessage) => {
 })
 
 function processCommand(receivedMessage) {
-    let fullCommand = receivedMessage.content.substr(prefix.length()) // Remove the leading exclamation mark
+    let fullCommand = receivedMessage.content.substr(prefix.length) // Remove the leading exclamation mark
     let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
     let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
     let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
 
     switch (primaryCommand){
-        case "Help":
-            helpCommand(receivedMessage);
-            break;
         case "help":
             helpCommand(receivedMessage);
-            break;
-        case "Time":
-            getTime(receivedMessage);
-            break;
-        case "time":
-            getTime(receivedMessage);
-            break;
-        case "Dark":
-            darkAuction(receivedMessage);
             break;
         case "dark":
             darkAuction(receivedMessage);
             break;
-        case "dmg":
-            dmgCalc(receivedMessage,arguments);
-            break;
         default:
-            receivedMessage.channel.send("Unbekannter Befehl. Der Befehl `!Help` gibt dir eine Übersicht der verfügbaren Befehle.");
+            receivedMessage.channel.send("Unbekannter Befehl. Der Befehl `!help` gibt dir eine Übersicht der verfügbaren Befehle.");
             break;
     }
 }
 
-function dmgCalc(receivedMessage,arguments){
-    let dmg = 0;
-    let weaponDmg = arguments[0];
-    let Strenght = arguments[1]/ 5;
-    let Strenght2 = arguments[1]/100;
-    let CritDmg = arguments[2]/100;
-    let CombatLvL = (arguments[3]*5);
-    dmg=(5+parseInt(weaponDmg)+parseInt(Strenght))*(1+parseInt(Strenght2))*(1+parseInt(CritDmg))*(1+(parseInt(CombatLvL)/100));
-    receivedMessage.channel.send("Dmg den du machst: "+dmg);
-}
 
 
 function helpCommand(receivedMessage) {    
-        receivedMessage.channel.send("'!help' Für die Übersicht der Befehle \n '!time' Für die aktuelle Uhrzeit \n '!dark' Für die verbleibende Zeit bis zur nächsten Dark Auction \n '!dmg WaffenDMG Strenght CritDmg CombatLvL' Für das berechnen des neuen DMG");
-}
-
-function getTime(receivedMessage){
-    var stunden, minuten
-  var StundenZahl, MinutenZahl
-
-  var heute = new Date()
-  StundenZahl = heute.getHours()
-  MinutenZahl = heute.getMinutes()
- 
-  stunden = StundenZahl+":";
-  if (MinutenZahl < 10) {minuten = "0" + MinutenZahl + ":"}
-    else {minuten = MinutenZahl }
-  zeit = "Es ist" + stunden + minuten + " Uhr"
-  receivedMessage.channel.send(zeit)
+        receivedMessage.channel.send("'!help' Für die Übersicht der Befehle \n '!dark' Für die verbleibende Zeit bis zur nächsten Dark Auction");
 }
 
 function darkAuction(receivedMessage){
