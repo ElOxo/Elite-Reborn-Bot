58be951eef1a0c759b54ccad3ab3e942ab286d47
Justus Rademacher <justus.berlin@gmail.com>
2020-05-19 13:30:48 +0200

    add pimmel command

---
+++ commands/pimmel.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: 'pimmel',
+	description: 'ICH BIN EIN BOT MIT PIMMEL',
+	guildOnly: true,
+	execute(message) {
+        message.reply('Du hast einen Kleinen Pimmel. ${message.author.username}')
+	},
+};
