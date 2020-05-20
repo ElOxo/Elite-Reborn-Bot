d9ae36816c6f27712fcd5c6af88d88c890bc508d
Justus Rademacher <justus.rademacher@bwb.de>
2020-05-20 12:19:29 +0200

    change embed

--- commands/startSupport.js
+++ commands/startSupport.js
@@ -5,13 +5,27 @@ module.exports = {
     execute(message) {
         // Wenn Support und Team Rang
         if (message.channel.id == 696821086846844969 && message.member.roles.has('697424990592696350')) {
-            let suggestembed = new Discord.RichEmbed()
-                .setTitle("Elite-Reborn Support")
-                .setDescription("Reagiere mit 📩 um deinen persönlichen Support Channel zu erstellen.")
-                .setColor("#00FFFF")
-                .setThumbnail('https://cdn.discordapp.com/avatars/712200237690650644/1d8ffdac4456cf8388d7b4941afb3661.png?size=2048')
-                .addFields({ name: 'Missbrauch ist Strafbar', value: 'ICH BANN EUCH WEG' });
-            message.channel.send({ embed: suggestedembed }).then(embedMessage => {
+
+            const exampleEmbed = {
+                color: 0x0099ff,
+                title: 'Elite-Reborn Support',
+                description: 'Reagiere mit 📩 um deinen persönlichen Support Channel zu erstellen.',
+                thumbnail: {
+                    url: 'https://cdn.discordapp.com/avatars/712200237690650644/1d8ffdac4456cf8388d7b4941afb3661.png?size=2048',
+                },
+                fields: [
+                    {
+                        name: 'Missbrauch ist Strafbar',
+                        value: 'ICH BANN EUCH WEG',
+                    }
+                ],
+                image: {
+                    url: 'https://cdn.discordapp.com/avatars/712200237690650644/1d8ffdac4456cf8388d7b4941afb3661.png?size=2048',
+                },
+                timestamp: new Date(),
+            };
+
+            message.channel.send({ embed: exampleEmbed }).then(embedMessage => {
                 embedMessage.react("📩");
             });
 
